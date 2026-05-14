f8b9fb39d76963cb2c15d22fa52e94ba1b285b5f
Leonwei1206 <68678844+Leonwei1206@users.noreply.github.com>
2026-05-14 17:15:13 +0800

    新增 start script

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,11 @@ require("dotenv").config();
 
 console.log(process.env.DATABASE_URL);
 const app = express();
+const PORT = process.env.PORT || 3000;
 
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 app.use(cors());
 app.use(express.json());
 
@@ -15,3 +19,7 @@ app.use("/api", routes);
 app.listen(3000, () => {
   console.log("Server running on http://localhost:3000");
 });
+
+app.use(cors({
+  origin: "https://Leonwei1206.github.io"
+}));
--- vueProject/vite.config.ts
+++ vueProject/vite.config.ts
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [
     vue(),tailwindcss(),
+  
 
-
-  ],
+  ],base: '/Vue3/',
 })
